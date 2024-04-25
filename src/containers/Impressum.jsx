fd1753293e7c372943af5611aa6046878c6cded7
Gorangmad <94233357+Gorangmad@users.noreply.github.com>
2024-04-25 20:11:02 +0200

    Update Impressum.jsx

--- src/containers/Impressum.jsx
+++ src/containers/Impressum.jsx
@@ -1,1 +1,34 @@
+import React from 'react';
+
+const Impressum = () => {
+    return (
+        <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
+            <h1>Impressum</h1>
+            <h2>Angaben gemäß § 5 TMG</h2>
+            <p>BKF Security and Services GmbH</p>
+            <p>Windhorststraße 62</p>
+            <p>65929, Frankfurt am Main</p>
+
+            <h2>Vertreten durch:</h2>
+            <p>Farooq Ahmed</p>
+
+            <h2>Kontakt:</h2>
+            <p>Telefon: +49 157 51140628</p>
+            <p>E-Mail: info@bkf-sec.com</p>
+
+            <h2>Umsatzsteuer-ID:</h2>
+            <p>Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz:</p>
+            <p>DE 123 456 789</p>
+
+            <h2>Haftung für Inhalte:</h2>
+            <p>Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.</p>
+            <p>Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
+
+            <h3>Haftung für Links:</h3>
+            <p>Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.</p>
+        </div>
+    );
+};
+
+export default Impressum;
 
