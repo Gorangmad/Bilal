b2d63c2ed7f25da63b825515698c26e837edd1a6
Gorangmad <94233357+Gorangmad@users.noreply.github.com>
2024-06-14 13:57:22 +0200

    Update Impressum.jsx

--- src/containers/Impressum.jsx
+++ src/containers/Impressum.jsx
@@ -18,7 +18,7 @@ const Impressum = () => {
 
             <h2>Umsatzsteuer-ID:</h2>
             <p>Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz:</p>
-            <p>DE 123 456 789</p>
+            <p>DE 365 932 643</p>
 
             <h2>Haftung für Inhalte:</h2>
             <p>Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.</p>
